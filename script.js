07ce69cb6abc5716b070674e4a40e11bda54f240
isaacgrube-netizen <isaac.grube@escola.pr.gov.br>
2026-08-31 09:19:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const desafios = [
+    "Resolver problemas de lógica",
+    "Aprender novas funções",
+    "Criar novas soluções",
+    "Aprender padrões",
+    "Criar uma invenção"
+];
+
+function iniciarDesafio(){
+    const name=document.getElementById("name")
+}f
